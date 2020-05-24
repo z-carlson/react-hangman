e59ed6f7c55a62d83de85df2a2c1e80e705dcd8b
z-carlson <zcarlson87@gmail.com>
2020-05-24 18:03:57 -0500

    Updates

---
+++ precache-manifest.fc40ead8d9420bdbba117519f9161898.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4dac13697128ee1cde1b964a50fd09f",
+    "url": "/react-hangman/index.html"
+  },
+  {
+    "revision": "d1cce5d2fe307bea7edf",
+    "url": "/react-hangman/static/css/main.908ca4ea.chunk.css"
+  },
+  {
+    "revision": "04d81a3b73a15f285cd6",
+    "url": "/react-hangman/static/js/2.68354abe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-hangman/static/js/2.68354abe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1cce5d2fe307bea7edf",
+    "url": "/react-hangman/static/js/main.dadd5535.chunk.js"
+  },
+  {
+    "revision": "04862e76dd907e8f4b6c",
+    "url": "/react-hangman/static/js/runtime-main.e1105988.js"
+  },
+  {
+    "revision": "db1659e9b4d2d48c36b452b939fa0b8f",
+    "url": "/react-hangman/static/media/circle0.db1659e9.svg"
+  },
+  {
+    "revision": "639bdc5cc85148696ffba52c93a4c6c1",
+    "url": "/react-hangman/static/media/circle1.639bdc5c.svg"
+  },
+  {
+    "revision": "2e57b6fb870f8cdb5bdf3f32045075e0",
+    "url": "/react-hangman/static/media/circle2.2e57b6fb.svg"
+  },
+  {
+    "revision": "9487d85631cb29f7f13b8a5f887c4955",
+    "url": "/react-hangman/static/media/circle3.9487d856.svg"
+  },
+  {
+    "revision": "003e7d4b0183d7554e76c93854573d4d",
+    "url": "/react-hangman/static/media/circle4.003e7d4b.svg"
+  },
+  {
+    "revision": "afa8aec3a1529ea575f703d31b792752",
+    "url": "/react-hangman/static/media/circle5.afa8aec3.svg"
+  },
+  {
+    "revision": "fc1f09fec7dadb45cde91ea6bd004091",
+    "url": "/react-hangman/static/media/circle6.fc1f09fe.svg"
+  },
+  {
+    "revision": "8ef73e707037cfe9b9c761ee493daa59",
+    "url": "/react-hangman/static/media/settings.8ef73e70.svg"
+  }
+]);
